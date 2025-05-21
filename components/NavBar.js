78fb1b86408281d25b82a93d71a1156bb1106ee4
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-21 18:04:07 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import LogoHamburger from "./LogoHamburger"; // Hamburger icon component
 
 const navItems = [
   { label: "PORTFOLIO", href: "/" },
@@ -11,33 +12,41 @@ export default function NavBar({ headerHeight = 76 }) {
     <nav
       style={{
         display: "flex",
-        gap: 46,
         alignItems: "center",
+        justifyContent: "space-between",
         height: headerHeight - 16,
         marginTop: 0,
+        paddingLeft: 24,
+        paddingRight: 24,
         fontFamily: "'Futura', 'Open Sans'",
       }}
     >
-      {navItems.map((item) => (
-        <Link key={item.href} href={item.href} passHref legacyBehavior>
-          <a
-            style={{
-              color: "#222C46",
-              fontWeight: 700,
-              fontSize: 22,
-              textDecoration: "none",
-              borderBottom: "2.5px solid transparent",
-              paddingBottom: 2,
-              letterSpacing: "1.7px",
-              transition: "border 0.16s, color 0.16s",
-            }}
-            onMouseOver={e => (e.currentTarget.style.borderBottom = "2.5px solid #222C46")}
-            onMouseOut={e => (e.currentTarget.style.borderBottom = "2.5px solid transparent")}
-          >
-            {item.label}
-          </a>
-        </Link>
-      ))}
+      {/* Navigation Links */}
+      <div style={{ display: "flex", gap: 46 }}>
+        {navItems.map((item) => (
+          <Link key={item.href} href={item.href} passHref>
+            <span
+              style={{
+                color: "#222C46",
+                fontWeight: 700,
+                fontSize: 22,
+                textDecoration: "none",
+                borderBottom: "2.5px solid transparent",
+                paddingBottom: 2,
+                letterSpacing: "1.7px",
+                cursor: "pointer",
+                transition: "border 0.16s, color 0.16s",
+              }}
+              onMouseOver={e => (e.currentTarget.style.borderBottom = "2.5px solid #222C46")}
+              onMouseOut={e => (e.currentTarget.style.borderBottom = "2.5px solid transparent")}
+            >
+              {item.label}
+            </span>
+          </Link>
+        ))}
+      </div>
+      {/* Hamburger Icon */}
+      <LogoHamburger />
     </nav>
   );
 }
