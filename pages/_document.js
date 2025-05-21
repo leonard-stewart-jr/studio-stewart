f60b225cb151678b8f8f9ff9699314c23a01d3df
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-21 17:30:35 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap"
           rel="stylesheet"
         />
+        {/* Favicon: SVG with PNG fallback */}
+        <link rel="icon" type="image/png" href="/assets/logo-mark-only-32.png" sizes="32x32" />
+        <link rel="icon" type="image/svg+xml" href="/assets/logo-mark-only.svg" />
       </Head>
       <body>
         <Main />
