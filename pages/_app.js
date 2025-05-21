e092cf50716efb15329f3db79203e841942d6a47
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-21 01:20:11 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import SplashScreen from "../components/SplashScreen";
+
+export default function MyApp({ Component, pageProps }) {
+  const [showSplash, setShowSplash] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!sessionStorage.getItem("splashShown")) {
+        setShowSplash(true);
+        sessionStorage.setItem("splashShown", "true");
+      }
+    }
+  }, []);
+
+  if (showSplash) {
+    return <SplashScreen onFinish={() => setShowSplash(false)} />;
+  }
+
+  return <Component {...pageProps} />;
+}
