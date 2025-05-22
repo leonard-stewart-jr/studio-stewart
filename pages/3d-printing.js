6453850b9f7bdd0930f698130f0cb051b3122582
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-22 00:04:20 -0500

    Update 3d-printing.js

--- pages/3d-printing.js
+++ pages/3d-printing.js
@@ -1,24 +1,19 @@
-import NavBar from "../components/NavBar";
-
 export default function ThreeDPrinting() {
   return (
-    <div>
-      <NavBar />
-      <main style={{
-        minHeight: '80vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: '#fafafa',
-        borderRadius: 12,
-        padding: '2rem',
-        margin: '0 auto',
-        maxWidth: 600,
-        boxShadow: "0 4px 24px #eee"
-      }}>
-        <h1 style={{ color: "#bbb", fontSize: "2.5rem" }}>IN PROGRESS</h1>
-      </main>
-    </div>
+    <main style={{
+      minHeight: '80vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#fafafa',
+      borderRadius: 12,
+      padding: '2rem',
+      margin: '0 auto',
+      maxWidth: 600,
+      boxShadow: "0 4px 24px #eee"
+    }}>
+      <h1 style={{ color: "#bbb", fontSize: "2.5rem" }}>IN PROGRESS</h1>
+    </main>
   );
 }
