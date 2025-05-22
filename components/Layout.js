d085118702218d670c9c93674f9d68e99a0a1c6e
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-21 23:08:18 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,39 +1,10 @@
-import LogoHamburger from "./LogoHamburger";
-// import NavBar from "./NavBar"; // Uncomment if you want to include your NavBar
+import HeaderBar from "./HeaderBar";
 
 export default function Layout({ children }) {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        width: "100vw",
-        display: "flex",
-        flexDirection: "column",
-        background: "#fff", // or your preferred background
-      }}
-    >
-      <header
-        style={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          padding: "0 0 0 8px",
-          background: "#fff", // or your preferred background
-          boxShadow: "0 2px 8px 0 rgba(0,0,0,0.02)",
-          zIndex: 1000,
-        }}
-      >
-        <LogoHamburger />
-        {/* <NavBar />  // Uncomment to add your navigation bar here */}
-      </header>
-      <main
-        style={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-        }}
-      >
+    <div style={{ minHeight: "100vh", width: "100vw", background: "#fff" }}>
+      <HeaderBar />
+      <main style={{ width: "100%", margin: "0 auto" }}>
         {children}
       </main>
     </div>
