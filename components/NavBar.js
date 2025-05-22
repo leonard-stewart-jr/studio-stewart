fbe62c879b08079660849411cee5d4b3b02d1165
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-22 13:22:28 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -31,14 +31,6 @@ export default function NavBar({ headerHeight = 76 }) {
         <Link key={item.href} href={item.href} passHref legacyBehavior>
           <a
             className={isActive(item.href) ? "active" : ""}
-            style={{
-              fontWeight: 700,
-              fontSize: 11, // Reduced font size to half of previous (was 22)
-              textDecoration: isActive(item.href) ? "underline" : "none",
-              padding: "4px 0", // Adjusted for smaller text
-              borderBottom: "1px solid #eee",
-              transition: "color 0.2s",
-            }}
           >
             {item.label}
           </a>
