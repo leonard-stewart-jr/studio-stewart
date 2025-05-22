0a7785aedc3a1ea436e1553a532b36e4f0286113
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-21 19:49:59 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import SplashScreen from "../components/SplashScreen";
 
 export default function MyApp({ Component, pageProps }) {
-  const [showSplash, setShowSplash] = useState(false);
+  const [showSplash, setShowSplash] = useState(() => {
+    // Prevent flash of main content before splash on first client render
+    if (typeof window !== "undefined") {
+      return !sessionStorage.getItem("splashShown");
+    }
+    return false; // On server, don't show splash
+  });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (!sessionStorage.getItem("splashShown")) {
-        setShowSplash(true);
-        sessionStorage.setItem("splashShown", "true");
-      }
+    if (showSplash && typeof window !== "undefined") {
+      sessionStorage.setItem("splashShown", "true");
     }
-  }, []);
+  }, [showSplash]);
 
   if (showSplash) {
     return <SplashScreen onFinish={() => setShowSplash(false)} />;
