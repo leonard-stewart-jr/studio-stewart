4f2e7442af9524198d0f1b8cf6309a2ddf182585
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-22 15:45:36 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import '../styles/sidebar.css';
 import { useEffect, useState } from "react";
 import SplashScreen from "../components/SplashScreen";
 import Layout from "../components/Layout";
