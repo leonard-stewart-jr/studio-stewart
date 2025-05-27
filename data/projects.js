739ad32621bfa1c8fbba6f979bd8b5bcb52b16da
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-05-27 12:51:46 -0500

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -5,11 +5,7 @@ const projects = [
     type: "Culture",
     slug: "urban-pavilion",
     description: "Project description goes here.",
-    media: [
-      { type: "video", src: "/videos/enscapeentry.mp4", caption: "Main Preview Video" },
-      { type: "image", src: "https://picsum.photos/800/450?random=10", caption: "Gallery Image 1" },
-      { type: "image", src: "https://picsum.photos/800/450?random=11", caption: "Gallery Image 2" }
-    ]
+    bannerSrc: "/images/urban-pavilion-banner.jpg"
   },
   {
     grade: "2nd Year",
@@ -17,11 +13,7 @@ const projects = [
     type: "Housing",
     slug: "residential-loft",
     description: "Project description goes here.",
-    media: [
-      { type: "video", src: "https://www.w3schools.com/html/movie.mp4", caption: "Main Preview Video" },
-      { type: "image", src: "https://picsum.photos/800/450?random=20", caption: "Gallery Image 1" },
-      { type: "image", src: "https://picsum.photos/800/450?random=21", caption: "Gallery Image 2" }
-    ]
+    bannerSrc: "/images/residential-loft-banner.jpg"
   },
   {
     grade: "4th Year",
@@ -29,11 +21,7 @@ const projects = [
     type: "Commercial",
     slug: "riverfront-studio-4th",
     description: "Project description goes here.",
-    media: [
-      { type: "video", src: "https://www.w3schools.com/html/mov_bbb.mp4", caption: "Main Preview Video" },
-      { type: "image", src: "https://picsum.photos/800/450?random=30", caption: "Gallery Image 1" },
-      { type: "image", src: "https://picsum.photos/800/450?random=31", caption: "Gallery Image 2" }
-    ]
+    bannerSrc: "/images/riverfront-studio-4th-banner.jpg"
   },
   {
     grade: "6th Year",
@@ -41,11 +29,7 @@ const projects = [
     type: "Commercial",
     slug: "riverfront-studio-6th",
     description: "Project description goes here.",
-    media: [
-      { type: "video", src: "https://www.w3schools.com/html/mov_bbb.mp4", caption: "Main Preview Video" },
-      { type: "image", src: "https://picsum.photos/800/450?random=32", caption: "Gallery Image 1" },
-      { type: "image", src: "https://picsum.photos/800/450?random=33", caption: "Gallery Image 2" }
-    ]
+    bannerSrc: "/images/riverfront-studio-6th-banner.jpg"
   },
   {
     grade: "1st Year",
@@ -53,12 +37,8 @@ const projects = [
     type: "Commercial",
     slug: "riverfront-studio-1st",
     description: "Project description goes here.",
-    media: [
-      { type: "video", src: "https://www.w3schools.com/html/mov_bbb.mp4", caption: "Main Preview Video" },
-      { type: "image", src: "https://picsum.photos/800/450?random=34", caption: "Gallery Image 1" },
-      { type: "image", src: "https://picsum.photos/800/450?random=35", caption: "Gallery Image 2" }
-    ]
-  },  
+    bannerSrc: "/images/riverfront-studio-1st-banner.jpg"
+  }
 ];
 
 export default projects;
