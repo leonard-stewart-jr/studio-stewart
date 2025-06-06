b8e190fda997c3cdcd8e1ae2d7e1da9f328774e9
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-06-06 14:33:46 -0500

    Update globe-section.js

--- components/isp/globe-section.js
+++ components/isp/globe-section.js
@@ -6,9 +6,40 @@ const Globe = dynamic(() => import("react-globe.gl"), { ssr: false });
 export default function GlobeSection({ onMarkerClick }) {
   const globeEl = useRef();
   return (
-    <section style={{ width: "100%", minHeight: "90vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", background: "#fff" }}>
-      <h2 style={{ marginTop: 32, marginBottom: 24, fontSize: 32, color: "#222" }}>World History: Prisons Across the Globe</h2>
-      <div style={{ width: 400, height: 400 }}>
+    <section
+      style={{
+        width: "100%",
+        minHeight: "480px",
+        background: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "32px",
+        paddingBottom: "0",
+      }}
+    >
+      <h2
+        style={{
+          marginTop: 0,
+          marginBottom: 16,
+          fontSize: 28,
+          fontWeight: 600,
+          color: "#222",
+          letterSpacing: "0.03em",
+        }}
+      >
+        World Timeline
+      </h2>
+      <div
+        style={{
+          width: 400,
+          height: 400,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginTop: "-24px", // Pull globe up
+        }}
+      >
         <Globe
           ref={globeEl}
           globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
