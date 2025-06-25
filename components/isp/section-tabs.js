e3e4e2df0bf1876ca609ae129b3348cad48db476
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-06-24 20:37:58 -0500

    Update section-tabs.js

--- components/isp/section-tabs.js
+++ components/isp/section-tabs.js
@@ -2,27 +2,25 @@ import React from "react";
 
 export default function SectionTabs({ activeSection, setActiveSection }) {
   return (
-    <div style={{ marginTop: 36, marginBottom: 0 }}>
-      <div className="isp-section-tabs">
-        <button
-          className={
-            "isp-tab-btn" + (activeSection === "history" ? " active" : "")
-          }
-          onClick={() => setActiveSection("history")}
-          aria-current={activeSection === "history" ? "page" : undefined}
-        >
-          HISTORY
-        </button>
-        <button
-          className={
-            "isp-tab-btn future" + (activeSection === "future" ? " active" : "")
-          }
-          onClick={() => setActiveSection("future")}
-          aria-current={activeSection === "future" ? "page" : undefined}
-        >
-          FUTURE
-        </button>
-      </div>
+    <div className="isp-section-tabs">
+      <button
+        className={
+          "isp-tab-btn" + (activeSection === "history" ? " active" : "")
+        }
+        onClick={() => setActiveSection("history")}
+        aria-current={activeSection === "history" ? "page" : undefined}
+      >
+        HISTORY
+      </button>
+      <button
+        className={
+          "isp-tab-btn" + (activeSection === "future" ? " active" : "")
+        }
+        onClick={() => setActiveSection("future")}
+        aria-current={activeSection === "future" ? "page" : undefined}
+      >
+        FUTURE
+      </button>
     </div>
   );
 }
