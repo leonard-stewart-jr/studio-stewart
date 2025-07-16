07728c8d5a945047fa032c9bc8db137734a0769d
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-07-15 21:19:16 -0500

    Update globe-section.js

--- components/isp/globe-section.js
+++ components/isp/globe-section.js
@@ -247,7 +247,7 @@ export default function GlobeSection({ onMarkerClick }) {
         // ALL STANDARD PINS
         if (obj.isStandardPin && pinModel) {
           const group = new THREE.Group();
-          const scale = 50;
+          const scale = 18;
           const pin = pinModel.clone(true);
           pin.traverse((child) => {
             if (child.isMesh) child.castShadow = false;
