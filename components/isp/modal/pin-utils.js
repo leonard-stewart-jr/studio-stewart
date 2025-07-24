32b7e451a99cf753a5385f88a2863c569793b966
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-07-24 18:28:14 -0500

    Update pin-utils.js

--- components/isp/modal/pin-utils.js
+++ components/isp/modal/pin-utils.js
@@ -51,7 +51,7 @@ export function orientPin(pin, markerVec) {
   const target = new THREE.Vector3(0, 0, 0);
   pin.lookAt(target);
 }
-export function positionPin(pin, markerVec, offset = -0.05) {
+export function positionPin(pin, markerVec, offset = -0.3) {
   const outwardVec = markerVec.clone().normalize().multiplyScalar(offset);
   pin.position.copy(outwardVec);
 }
