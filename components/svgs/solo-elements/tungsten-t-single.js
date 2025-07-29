f6852258a27f554afa552172573a8d587602289b
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-07-29 10:56:04 -0500

    Update tungsten-t-single.js

--- components/svgs/solo-elements/tungsten-t-single.js
+++ components/svgs/solo-elements/tungsten-t-single.js
@@ -2,7 +2,8 @@ import * as React from "react";
 
 export default function TungstenTSingle({ style, title }) {
   return (
-<svg viewBox="0 0 52.65 52.65" width={52.65} height={52.65}>
+    <svg
+      viewBox="0 0 52.65 52.65"
       style={style}
       aria-label={title}
       role="img"
@@ -12,53 +13,51 @@ export default function TungstenTSingle({ style, title }) {
         y="0"
         width="52.65"
         height="52.65"
-        rx="8"
+        rx="7"
         fill="rgba(192,57,43,.35)"
         stroke="#f5db12"
         strokeWidth="3"
         strokeLinejoin="bevel"
       />
       <text
-        x="50%"
-        y="24%"
-        dominantBaseline="middle"
-        textAnchor="middle"
-        fontSize="11"
+        x="6.2"
+        y="12"
+        fontSize="8"
         fontWeight="300"
         fill="#181818"
+        fontFamily="Open Sans, Arial, sans-serif"
       >
         74
       </text>
       <text
-        x="50%"
-        y="56%"
-        dominantBaseline="middle"
+        x="26.33"
+        y="32"
         textAnchor="middle"
-        fontSize="24"
+        fontSize="18"
         fontWeight="700"
         fill="#181818"
+        fontFamily="Open Sans, Arial, sans-serif"
       >
         W
       </text>
       <text
-        x="50%"
-        y="72%"
-        dominantBaseline="middle"
-        textAnchor="middle"
-        fontSize="9"
-        fontWeight="300"
-        fill="#181818"
-      >
-        Tungsten
-      </text>
-      <text
-        x="50%"
-        y="88%"
-        dominantBaseline="middle"
+        x="26.33"
+        y="43"
         textAnchor="middle"
         fontSize="8"
         fontWeight="300"
         fill="#181818"
+        fontFamily="Open Sans, Arial, sans-serif"
+      >
+        Tungsten
+      </text>
+      <text
+        x="20"
+        y="50"
+        fontSize="7"
+        fontWeight="300"
+        fill="#181818"
+        fontFamily="Open Sans, Arial, sans-serif"
       >
         183.84
       </text>
