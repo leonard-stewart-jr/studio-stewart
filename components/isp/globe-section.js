c0ea4e6e934cb105278bbbbc53602565b1e919d2
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2025-08-14 19:46:45 -0500

    Update globe-section.js

--- components/isp/globe-section.js
+++ components/isp/globe-section.js
@@ -216,7 +216,7 @@ export default function GlobeSection({ onMarkerClick, mode = "world" }) {
 
             // Orient the flag so that its "banner" is upright on the globe
             orientPin(flag, markerVec);
-            flag.rotateX(-Math.PI / 2); // Axis fix if Y/Z are flipped in your model
+            flag.rotateY(-Math.PI / 2); // Axis fix if Y/Z are flipped in your model
 
             positionPin(flag, -8);
 
