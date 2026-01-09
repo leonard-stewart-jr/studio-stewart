f9c0526591690ddedaf112de32d5e7a3db789b80
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2026-01-08 21:07:52 -0600

    Update HeaderBar.js

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -50,6 +50,7 @@ export default function HeaderBar({ fixedNav = false }) {
             transition={hamburgerTransition}
             style={{
               marginLeft: sidebarPaddingLeft,
+              marginTop: -10, // shift logo up by 10px
               cursor: "pointer",
               opacity: sidebarOpen ? 0 : 1,
               pointerEvents: sidebarOpen ? "none" : "auto",
