7054184c8f68d732e31e795046e9ed9a87e4c61c
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2026-01-09 12:52:38 -0600

    Change font to Inter and update favicon links
    Updated font links and favicon references.

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=Bungee+Shade&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet" 
-/>
+        {/* Inter variable font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+
         {/* Favicon: PNG fallback and SVG */}
-        <link rel="icon" type="image/png" href="/assets/logo-mark-only-32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="icon" type="image/svg+xml" href="/assets/logo-mark-only.svg" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
