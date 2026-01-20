358bb22611298b15364894e957a78f6612802399
leonard-stewart-jr <leonard.stewart@studio-stewart.com>
2026-01-19 18:11:21 -0600

    Adjust navOffset in FloatingProjectModal

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
           onClose={() => setActiveIndex(null)}
           {...getProjectModalProps(projects[activeIndex])}
           height={785}
-          navOffset={76}
+          navOffset={60}
         />
       )}
     </>
