8de7067695e5baa5041aad310bea7ec9e3afb370
leonard-stewart-jr <leonardwaynejr@icloud.com>
2026-08-17 13:31:22 -0500

    Allow Safari gesture events for custom portfolio zoom

--- pages/undergraduate-portfolio.js
+++ pages/undergraduate-portfolio.js
@@ -65,7 +65,7 @@ export default function UndergraduatePortfolioPage() {
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
           />
         </Head>
       )}
