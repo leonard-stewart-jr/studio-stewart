c47863a08f921e8acc1527b5583664951e19d068
leonard-stewart-jr <leonardwaynejr@icloud.com>
2026-08-17 22:58:25 -0500

    Load sports league dropdown handle styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "../styles/portfolio-page-backgrounds.css";
 import "../styles/three-d-printing-fixes.css";
+import "../styles/three-d-printing-league-toggle.css";
 import { useEffect, useState } from "react";
 import SplashScreen from "../components/SplashScreen";
 import Layout from "../components/Layout";
