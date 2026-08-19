bca231eab7e7dc7ed35e1743e8e5967365456394
leonard-stewart-jr <leonardwaynejr@icloud.com>
2026-08-18 20:15:23 -0500

    Match Tin overlay to master periodic table

--- components/svgs/solo-elements/tin-sn-single.js
+++ components/svgs/solo-elements/tin-sn-single.js
@@ -2,64 +2,58 @@ import * as React from "react";
 
 export default function TinSnSingle(props) {
   return (
-    <svg
-      viewBox="0 0 61 61"
-      width="100%"
-      height="100%"
-      {...props}
-    >
+    <svg viewBox="0 0 63.59 63.59" width="100%" height="100%" {...props}>
       <rect
-        x="0"
-        y="0"
-        width="61"
-        height="61"
-        rx="8"
+        x="1.5"
+        y="1.5"
+        width="60.59"
+        height="60.59"
         fill="rgba(192,57,43,.35)"
         stroke="#54b948"
         strokeWidth="3"
         strokeLinejoin="bevel"
       />
+
       <text
-        x="50%"
-        y="24%"
-        dominantBaseline="middle"
-        textAnchor="middle"
+        x="4.46"
+        y="12.64"
         fontSize="11"
         fontWeight="300"
         fill="#181818"
+        fontFamily='OpenSans-Light, "Open Sans"'
       >
         50
       </text>
+
       <text
-        x="50%"
-        y="56%"
-        dominantBaseline="middle"
-        textAnchor="middle"
+        x="17.19"
+        y="35.37"
         fontSize="24"
         fontWeight="700"
         fill="#181818"
+        fontFamily='OpenSans-Bold, "Open Sans"'
       >
         Sn
       </text>
+
       <text
-        x="50%"
-        y="72%"
-        dominantBaseline="middle"
-        textAnchor="middle"
+        x="26.5"
+        y="45.71"
         fontSize="9"
         fontWeight="300"
         fill="#181818"
+        fontFamily='OpenSans-Light, "Open Sans"'
       >
         Tin
       </text>
+
       <text
-        x="50%"
-        y="88%"
-        dominantBaseline="middle"
-        textAnchor="middle"
+        x="18.16"
+        y="57.17"
         fontSize="8"
         fontWeight="300"
         fill="#181818"
+        fontFamily='OpenSans-Light, "Open Sans"'
       >
         118.711
       </text>
