ccbe37b7fa799fb15438c3e022cb8098f7a985aa
leonard-stewart-jr <leonardwaynejr@icloud.com>
2026-08-23 20:07:31 -0500

    Load mobile 3D printing layout fixes

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/portfolio-page-backgrounds.css";
 import "../styles/three-d-printing-fixes.css";
 import "../styles/three-d-printing-league-toggle.css";
 import "../styles/three-d-printing-logo-alignment.css";
+import "../styles/three-d-printing-mobile.css";
 import { useEffect, useState } from "react";
 import SplashScreen from "../components/SplashScreen";
 import Layout from "../components/Layout";
